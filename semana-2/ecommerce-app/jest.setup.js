739f443319d308c4a8c929ac323ebbed2359c618
Humberto Rivero <humbertorivero9@gmail.com>
2023-08-04 16:00:36 -0300

    Delete file types.d.ts

--- semana-2/ecommerce-app/jest.setup.js
+++ semana-2/ecommerce-app/jest.setup.js
@@ -4,17 +4,3 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
-// tests/test-setup.js
-// import { useRouter } from 'next/router';
-
-// jest.mock('next/router', () => ({
-//   useRouter: jest.fn(),
-// }));
-
-// const mockRouter = {
-//   // Agrega las propiedades del router que estés utilizando en la página Home
-//   // Por ejemplo, si necesitas 'locale', agrégalo aquí.
-//   locale: 'es-ES',
-// };
-
-// useRouter.mockImplementation(() => mockRouter);
