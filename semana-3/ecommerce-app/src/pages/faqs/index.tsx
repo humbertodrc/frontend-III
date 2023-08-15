418edf83d07f2fd7dc5daf052f2ad5a936c93996
Humberto Rivero <humbertorivero9@gmail.com>
2023-08-15 14:19:36 -0300

    Fix API url

--- semana-3/ecommerce-app/src/pages/faqs/index.tsx
+++ semana-3/ecommerce-app/src/pages/faqs/index.tsx
@@ -1,41 +1,42 @@
-import { Layout } from '@/components/layouts/Layout'
-import { Faq } from '@/interface/faqs'
-import { NextPage } from 'next'
-import styles from './Faqs.module.css'
+// import { Layout } from '@/components/layouts/Layout'
+// import { Faq } from '@/interface/faqs'
+// import { NextPage } from 'next'
+// import styles from './Faqs.module.css'
 
-interface Props{
-  faqs: Faq[]
-}
+// interface Props{
+//   faqs: Faq[]
+// }
 
 
-const faqsPage: NextPage<Props> = ({ faqs }) => {
+// const faqsPage: NextPage<Props> = ({ faqs }) => {
 
-  return (
-    <Layout title="Preguntas Frecuentes">
-      <div className={styles.container}>
-        {faqs.map((faq) => (
-          <div key={faq.id}>
-            <h2>{faq.question}</h2>
-            <p>{faq.answer}</p>
-          </div>
-          ))}
-      </div>
-     </Layout>
-  )
-}
+//   return (
+//     <Layout title="Preguntas Frecuentes">
+//       <div className={styles.container}>
+//         {faqs.map((faq) => (
+//           <div key={faq.id}>
+//             <h2>{faq.question}</h2>
+//             <p>{faq.answer}</p>
+//           </div>
+//           ))}
+//       </div>
+//      </Layout>
+//   )
+// }
 
-export const getStaticProps = async () => {
+// export const getStaticProps = async () => {
 
-  // Modificar la url por las que nos da Vercel al hacer deploy
-  const response = await fetch('https://frontend-fvme6p1h1-danieldamian09.vercel.app/api/faqs')
-  const faqs = await response.json()
+//   // Modificar la url por las que nos da Vercel al hacer deploy
+//   const response = await fetch('http://localhost:3000/api/faqs')
+//   const faqs = await response.json()
 
-  return {
-    props: {
-      faqs
-    }
-  }
+//   return {
+//     props: {
+//       faqs
+//     }
+//   }
 
-}
+// }
 
-export default faqsPage
+// export default faqsPage
+
